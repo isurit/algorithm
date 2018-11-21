dbc681a63ae46dbf66a6927e6b12a5b3cdaf5c8d
리다 <isurit@lidaui-MBP.lan>
2018-11-21 23:33:52 +0900

    Init

---
+++ M1805/main.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
+
+    freopen("/Users/isurit/Documents/Code/M1805/M1805/input.txt", "r", stdin);
+    cin>>T;
+    T = 1;
+    
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        cout<<"# " << test_case<< " ";
+    }
+    return 0;
+}
